d25d1a2a8c722aa5952a65526ad690ff934a0d21
Juan Thomas Alvarez <alvarezjt@hotmail.com>
2020-07-26 10:54:02 -0400

    Add project files.

---
+++ WebCronW10/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WebCronW10
+{
+    partial class FormWebCronMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.panelBottom = new System.Windows.Forms.Panel();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.panelCenter = new System.Windows.Forms.Panel();
+            this.panelTopBorder = new System.Windows.Forms.Panel();
+            this.panelLeftBorder = new System.Windows.Forms.Panel();
+            this.panelCenterBorder = new System.Windows.Forms.Panel();
+            this.panelRightBorder = new System.Windows.Forms.Panel();
+            this.panelBottomBorder = new System.Windows.Forms.Panel();
+            this.panelRightcontent = new System.Windows.Forms.Panel();
+            this.panelTop.SuspendLayout();
+            this.panelBottom.SuspendLayout();
+            this.panelLeft.SuspendLayout();
+            this.panelRight.SuspendLayout();
+            this.panelCenter.SuspendLayout();
+            this.panelRightBorder.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTop
+            // 
+            this.panelTop.Controls.Add(this.panelTopBorder);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(1159, 30);
+            this.panelTop.TabIndex = 0;
+            // 
+            // panelBottom
+            // 
+            this.panelBottom.Controls.Add(this.panelBottomBorder);
+            this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelBottom.Location = new System.Drawing.Point(0, 627);
+            this.panelBottom.Name = "panelBottom";
+            this.panelBottom.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
+            this.panelBottom.Size = new System.Drawing.Size(1159, 30);
+            this.panelBottom.TabIndex = 1;
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.Controls.Add(this.panelLeftBorder);
+            this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeft.Location = new System.Drawing.Point(0, 30);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Padding = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.panelLeft.Size = new System.Drawing.Size(300, 597);
+            this.panelLeft.TabIndex = 2;
+            // 
+            // panelRight
+            // 
+            this.panelRight.BackColor = System.Drawing.Color.Transparent;
+            this.panelRight.Controls.Add(this.panelRightBorder);
+            this.panelRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelRight.Location = new System.Drawing.Point(859, 30);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Padding = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.panelRight.Size = new System.Drawing.Size(300, 597);
+            this.panelRight.TabIndex = 3;
+            // 
+            // panelCenter
+            // 
+            this.panelCenter.Controls.Add(this.panelCenterBorder);
+            this.panelCenter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelCenter.Location = new System.Drawing.Point(300, 30);
+            this.panelCenter.Name = "panelCenter";
+            this.panelCenter.Size = new System.Drawing.Size(559, 597);
+            this.panelCenter.TabIndex = 4;
+            // 
+            // panelTopBorder
+            // 
+            this.panelTopBorder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelTopBorder.Location = new System.Drawing.Point(0, 0);
+            this.panelTopBorder.Name = "panelTopBorder";
+            this.panelTopBorder.Size = new System.Drawing.Size(1159, 30);
+            this.panelTopBorder.TabIndex = 0;
+            // 
+            // panelLeftBorder
+            // 
+            this.panelLeftBorder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelLeftBorder.Location = new System.Drawing.Point(5, 0);
+            this.panelLeftBorder.Name = "panelLeftBorder";
+            this.panelLeftBorder.Size = new System.Drawing.Size(290, 597);
+            this.panelLeftBorder.TabIndex = 0;
+            // 
+            // panelCenterBorder
+            // 
+            this.panelCenterBorder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelCenterBorder.Location = new System.Drawing.Point(0, 0);
+            this.panelCenterBorder.Name = "panelCenterBorder";
+            this.panelCenterBorder.Size = new System.Drawing.Size(559, 597);
+            this.panelCenterBorder.TabIndex = 0;
+            // 
+            // panelRightBorder
+            // 
+            this.panelRightBorder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panelRightBorder.Controls.Add(this.panelRightcontent);
+            this.panelRightBorder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelRightBorder.Location = new System.Drawing.Point(5, 0);
+            this.panelRightBorder.Margin = new System.Windows.Forms.Padding(10);
+            this.panelRightBorder.Name = "panelRightBorder";
+            this.panelRightBorder.Padding = new System.Windows.Forms.Padding(1);
+            this.panelRightBorder.Size = new System.Drawing.Size(290, 597);
+            this.panelRightBorder.TabIndex = 0;
+            // 
+            // panelBottomBorder
+            // 
+            this.panelBottomBorder.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panelBottomBorder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBottomBorder.Location = new System.Drawing.Point(0, 5);
+            this.panelBottomBorder.Name = "panelBottomBorder";
+            this.panelBottomBorder.Size = new System.Drawing.Size(1159, 25);
+            this.panelBottomBorder.TabIndex = 0;
+            // 
+            // panelRightcontent
+            // 
+            this.panelRightcontent.BackColor = System.Drawing.Color.White;
+            this.panelRightcontent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelRightcontent.Location = new System.Drawing.Point(1, 1);
+            this.panelRightcontent.Name = "panelRightcontent";
+            this.panelRightcontent.Size = new System.Drawing.Size(288, 595);
+            this.panelRightcontent.TabIndex = 0;
+            // 
+            // FormWebCronMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1159, 657);
+            this.Controls.Add(this.panelCenter);
+            this.Controls.Add(this.panelRight);
+            this.Controls.Add(this.panelLeft);
+            this.Controls.Add(this.panelBottom);
+            this.Controls.Add(this.panelTop);
+            this.Name = "FormWebCronMain";
+            this.Text = "Web Cron for Windows 10";
+            this.panelTop.ResumeLayout(false);
+            this.panelBottom.ResumeLayout(false);
+            this.panelLeft.ResumeLayout(false);
+            this.panelRight.ResumeLayout(false);
+            this.panelCenter.ResumeLayout(false);
+            this.panelRightBorder.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Panel panelBottom;
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.Panel panelRight;
+        private System.Windows.Forms.Panel panelCenter;
+        private System.Windows.Forms.Panel panelTopBorder;
+        private System.Windows.Forms.Panel panelBottomBorder;
+        private System.Windows.Forms.Panel panelLeftBorder;
+        private System.Windows.Forms.Panel panelRightBorder;
+        private System.Windows.Forms.Panel panelCenterBorder;
+        private System.Windows.Forms.Panel panelRightcontent;
+    }
+}
+
