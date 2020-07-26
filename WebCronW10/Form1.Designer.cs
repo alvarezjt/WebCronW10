05ce40e692e9f9e2d2c1c1e801b3502d5a242b11
Juan Thomas Alvarez <alvarezjt@hotmail.com>
2020-07-26 11:20:12 -0400

    Updated main form.

--- WebCronW10/Form1.Designer.cs
+++ WebCronW10/Form1.Designer.cs
@@ -29,22 +29,37 @@
         private void InitializeComponent()
         {
             this.panelTop = new System.Windows.Forms.Panel();
-            this.panelBottom = new System.Windows.Forms.Panel();
-            this.panelLeft = new System.Windows.Forms.Panel();
-            this.panelRight = new System.Windows.Forms.Panel();
-            this.panelCenter = new System.Windows.Forms.Panel();
             this.panelTopBorder = new System.Windows.Forms.Panel();
-            this.panelLeftBorder = new System.Windows.Forms.Panel();
-            this.panelCenterBorder = new System.Windows.Forms.Panel();
-            this.panelRightBorder = new System.Windows.Forms.Panel();
+            this.panelTopContent = new System.Windows.Forms.Panel();
+            this.panelBottom = new System.Windows.Forms.Panel();
             this.panelBottomBorder = new System.Windows.Forms.Panel();
+            this.panelBottomContent = new System.Windows.Forms.Panel();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.panelLeftBorder = new System.Windows.Forms.Panel();
+            this.panelLeftContent = new System.Windows.Forms.Panel();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.panelRightBorder = new System.Windows.Forms.Panel();
             this.panelRightcontent = new System.Windows.Forms.Panel();
+            this.panelCenter = new System.Windows.Forms.Panel();
+            this.panelCenterBorder = new System.Windows.Forms.Panel();
+            this.panelCenterContent = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panelTop.SuspendLayout();
+            this.panelTopBorder.SuspendLayout();
+            this.panelTopContent.SuspendLayout();
             this.panelBottom.SuspendLayout();
+            this.panelBottomBorder.SuspendLayout();
             this.panelLeft.SuspendLayout();
+            this.panelLeftBorder.SuspendLayout();
             this.panelRight.SuspendLayout();
-            this.panelCenter.SuspendLayout();
             this.panelRightBorder.SuspendLayout();
+            this.panelCenter.SuspendLayout();
+            this.panelCenterBorder.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelTop
@@ -56,6 +71,24 @@
             this.panelTop.Size = new System.Drawing.Size(1159, 30);
             this.panelTop.TabIndex = 0;
             // 
+            // panelTopBorder
+            // 
+            this.panelTopBorder.Controls.Add(this.panelTopContent);
+            this.panelTopBorder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelTopBorder.Location = new System.Drawing.Point(0, 0);
+            this.panelTopBorder.Name = "panelTopBorder";
+            this.panelTopBorder.Size = new System.Drawing.Size(1159, 30);
+            this.panelTopBorder.TabIndex = 0;
+            // 
+            // panelTopContent
+            // 
+            this.panelTopContent.Controls.Add(this.menuStrip1);
+            this.panelTopContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelTopContent.Location = new System.Drawing.Point(0, 0);
+            this.panelTopContent.Name = "panelTopContent";
+            this.panelTopContent.Size = new System.Drawing.Size(1159, 30);
+            this.panelTopContent.TabIndex = 0;
+            // 
             // panelBottom
             // 
             this.panelBottom.Controls.Add(this.panelBottomBorder);
@@ -65,6 +98,24 @@
             this.panelBottom.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
             this.panelBottom.Size = new System.Drawing.Size(1159, 30);
             this.panelBottom.TabIndex = 1;
+            // 
+            // panelBottomBorder
+            // 
+            this.panelBottomBorder.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panelBottomBorder.Controls.Add(this.panelBottomContent);
+            this.panelBottomBorder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBottomBorder.Location = new System.Drawing.Point(0, 5);
+            this.panelBottomBorder.Name = "panelBottomBorder";
+            this.panelBottomBorder.Size = new System.Drawing.Size(1159, 25);
+            this.panelBottomBorder.TabIndex = 0;
+            // 
+            // panelBottomContent
+            // 
+            this.panelBottomContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBottomContent.Location = new System.Drawing.Point(0, 0);
+            this.panelBottomContent.Name = "panelBottomContent";
+            this.panelBottomContent.Size = new System.Drawing.Size(1159, 25);
+            this.panelBottomContent.TabIndex = 0;
             // 
             // panelLeft
             // 
@@ -76,6 +127,26 @@
             this.panelLeft.Size = new System.Drawing.Size(300, 597);
             this.panelLeft.TabIndex = 2;
             // 
+            // panelLeftBorder
+            // 
+            this.panelLeftBorder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panelLeftBorder.Controls.Add(this.panelLeftContent);
+            this.panelLeftBorder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelLeftBorder.Location = new System.Drawing.Point(5, 0);
+            this.panelLeftBorder.Name = "panelLeftBorder";
+            this.panelLeftBorder.Padding = new System.Windows.Forms.Padding(1);
+            this.panelLeftBorder.Size = new System.Drawing.Size(290, 597);
+            this.panelLeftBorder.TabIndex = 0;
+            // 
+            // panelLeftContent
+            // 
+            this.panelLeftContent.BackColor = System.Drawing.Color.White;
+            this.panelLeftContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelLeftContent.Location = new System.Drawing.Point(1, 1);
+            this.panelLeftContent.Name = "panelLeftContent";
+            this.panelLeftContent.Size = new System.Drawing.Size(288, 595);
+            this.panelLeftContent.TabIndex = 0;
+            // 
             // panelRight
             // 
             this.panelRight.BackColor = System.Drawing.Color.Transparent;
@@ -86,39 +157,6 @@
             this.panelRight.Padding = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.panelRight.Size = new System.Drawing.Size(300, 597);
             this.panelRight.TabIndex = 3;
-            // 
-            // panelCenter
-            // 
-            this.panelCenter.Controls.Add(this.panelCenterBorder);
-            this.panelCenter.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelCenter.Location = new System.Drawing.Point(300, 30);
-            this.panelCenter.Name = "panelCenter";
-            this.panelCenter.Size = new System.Drawing.Size(559, 597);
-            this.panelCenter.TabIndex = 4;
-            // 
-            // panelTopBorder
-            // 
-            this.panelTopBorder.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelTopBorder.Location = new System.Drawing.Point(0, 0);
-            this.panelTopBorder.Name = "panelTopBorder";
-            this.panelTopBorder.Size = new System.Drawing.Size(1159, 30);
-            this.panelTopBorder.TabIndex = 0;
-            // 
-            // panelLeftBorder
-            // 
-            this.panelLeftBorder.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelLeftBorder.Location = new System.Drawing.Point(5, 0);
-            this.panelLeftBorder.Name = "panelLeftBorder";
-            this.panelLeftBorder.Size = new System.Drawing.Size(290, 597);
-            this.panelLeftBorder.TabIndex = 0;
-            // 
-            // panelCenterBorder
-            // 
-            this.panelCenterBorder.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelCenterBorder.Location = new System.Drawing.Point(0, 0);
-            this.panelCenterBorder.Name = "panelCenterBorder";
-            this.panelCenterBorder.Size = new System.Drawing.Size(559, 597);
-            this.panelCenterBorder.TabIndex = 0;
             // 
             // panelRightBorder
             // 
@@ -132,15 +170,6 @@
             this.panelRightBorder.Size = new System.Drawing.Size(290, 597);
             this.panelRightBorder.TabIndex = 0;
             // 
-            // panelBottomBorder
-            // 
-            this.panelBottomBorder.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.panelBottomBorder.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelBottomBorder.Location = new System.Drawing.Point(0, 5);
-            this.panelBottomBorder.Name = "panelBottomBorder";
-            this.panelBottomBorder.Size = new System.Drawing.Size(1159, 25);
-            this.panelBottomBorder.TabIndex = 0;
-            // 
             // panelRightcontent
             // 
             this.panelRightcontent.BackColor = System.Drawing.Color.White;
@@ -149,6 +178,74 @@
             this.panelRightcontent.Name = "panelRightcontent";
             this.panelRightcontent.Size = new System.Drawing.Size(288, 595);
             this.panelRightcontent.TabIndex = 0;
+            // 
+            // panelCenter
+            // 
+            this.panelCenter.Controls.Add(this.panelCenterBorder);
+            this.panelCenter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelCenter.Location = new System.Drawing.Point(300, 30);
+            this.panelCenter.Name = "panelCenter";
+            this.panelCenter.Size = new System.Drawing.Size(559, 597);
+            this.panelCenter.TabIndex = 4;
+            // 
+            // panelCenterBorder
+            // 
+            this.panelCenterBorder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panelCenterBorder.Controls.Add(this.panelCenterContent);
+            this.panelCenterBorder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelCenterBorder.Location = new System.Drawing.Point(0, 0);
+            this.panelCenterBorder.Name = "panelCenterBorder";
+            this.panelCenterBorder.Padding = new System.Windows.Forms.Padding(1);
+            this.panelCenterBorder.Size = new System.Drawing.Size(559, 597);
+            this.panelCenterBorder.TabIndex = 0;
+            // 
+            // panelCenterContent
+            // 
+            this.panelCenterContent.BackColor = System.Drawing.Color.White;
+            this.panelCenterContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelCenterContent.Location = new System.Drawing.Point(1, 1);
+            this.panelCenterContent.Name = "panelCenterContent";
+            this.panelCenterContent.Size = new System.Drawing.Size(557, 595);
+            this.panelCenterContent.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1159, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
             // 
             // FormWebCronMain
             // 
@@ -161,14 +258,23 @@
             this.Controls.Add(this.panelLeft);
             this.Controls.Add(this.panelBottom);
             this.Controls.Add(this.panelTop);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "FormWebCronMain";
             this.Text = "Web Cron for Windows 10";
             this.panelTop.ResumeLayout(false);
+            this.panelTopBorder.ResumeLayout(false);
+            this.panelTopContent.ResumeLayout(false);
+            this.panelTopContent.PerformLayout();
             this.panelBottom.ResumeLayout(false);
+            this.panelBottomBorder.ResumeLayout(false);
             this.panelLeft.ResumeLayout(false);
+            this.panelLeftBorder.ResumeLayout(false);
             this.panelRight.ResumeLayout(false);
-            this.panelCenter.ResumeLayout(false);
             this.panelRightBorder.ResumeLayout(false);
+            this.panelCenter.ResumeLayout(false);
+            this.panelCenterBorder.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -186,6 +292,15 @@
         private System.Windows.Forms.Panel panelRightBorder;
         private System.Windows.Forms.Panel panelCenterBorder;
         private System.Windows.Forms.Panel panelRightcontent;
+        private System.Windows.Forms.Panel panelTopContent;
+        private System.Windows.Forms.Panel panelBottomContent;
+        private System.Windows.Forms.Panel panelLeftContent;
+        private System.Windows.Forms.Panel panelCenterContent;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
     }
 }
 
